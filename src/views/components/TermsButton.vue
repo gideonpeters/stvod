fb839e4655f7677b15987582d5528ea0fe77462e
GIDEON PETERS <gideonpeters85@gmail.com>
2019-10-22 14:40:44 +0100

    made some changes

---
+++ src/views/components/TermsButton.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<div
+			class="cursor-pointer rounded-bl-full px-20 py-8 bg-white text-black shadow-md absolute right-0 bottom-0 mb-4 font-semibold"
+		>Terms, Privacy, FAQs & more...</div>
+	</div>
+</template>
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,27 +1,34 @@
 <template>
-	<div class="home">
+	<div class="home flex h-screen">
 		<div class="bg-darken"></div>
 		<div class="main-body">
-			<div class="mx-16 flex flex-row-reverse">
+			<div class="flex flex-row-reverse absolute right-0 mr-16">
 				<div
-					class="text-white text-xl px-10 py-6 uppercase"
+					class="text-white text-xl px-10 py-6 uppercase cursor-pointer"
 					v-for="(route, ix) in routes"
 					:key="ix"
 				>{{route.title}}</div>
 			</div>
 
-			<div>
-				<div class="text-white text-5xl px-64">Get access to thousands of TV shows and movies on the go.</div>
-				<div class="px-64">
-					<input type="text" placeholder="Search" class="movie-search" />
+			<div class="flex flex-col justify-center h-full">
+				<div
+					class="text-white text-5xl px-64 font-semibold"
+				>Get access to thousands of TV shows and movies on the go.</div>
+				<div class="px-64 mt-5">
+					<input type="text" placeholder="Search" class="movie-search text-2xl" />
 				</div>
+				<terms-button />
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import TermsButton from "./../components/TermsButton";
 export default {
+	components: {
+		TermsButton
+	},
 	data: () => ({
 		routes: [
 			{
@@ -55,7 +62,6 @@ export default {
 	background-position: center center;
 	background-repeat: no-repeat;
 	background-size: cover;
-	height: 100vh;
 	overflow: hidden;
 	margin: 0;
 	padding: 0;
@@ -81,6 +87,12 @@ export default {
 			border-radius: 8px;
 			padding: 30px;
 			width: 100%;
+			&:active,
+			:hover,
+			:focus,
+			.active {
+				outline: none !important;
+			}
 		}
 	}
 }
